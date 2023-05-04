a9c93a5d40e90bd674dbb00e0244c4d7ea9fd623
Patrick McMichael <patrick@patrickmcmichael.org>
2023-05-04 16:49:54 +0100

    Initial commit

---
+++ FunctionsAbcXyz/AbcFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionsAbcXyz;
+
+public static class AbcFunction
+{
+    [FunctionName("AbcFunction")]
+    public static Task RunAsync([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+    {
+        log.LogInformation("C# Timer trigger function executed at: {UtcNow}", DateTime.UtcNow);
+        return Task.CompletedTask;
+    }
+}
